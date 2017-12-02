d431828a24fb840e119529f4eedb4ce9f6ec21b4
Timothymartin <32103186+Timothymartin@users.noreply.github.com>
2017-12-02 17:39:37 -0500

    n/a

--- System/game/system/_Attacker.java
+++ System/game/system/_Attacker.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package game.system;
 import game.models.Actor;
 import game.models.Attacker;
@@ -20,26 +20,3 @@ public class _Attacker extends _Actor implements Attacker
     }
     protected _Attacker clone() { return (_Attacker)super.clone(); }
 }
-=======
-package game.system;
-import game.models.Actor;
-import game.models.Attacker;
-import game.models.Node;
-import java.util.List;
-
-public class _Attacker extends _Actor implements Attacker
-{
-    public List<Integer> getPossibleDirs(boolean canReverse) { return super.getPossibleDirs(canReverse); }
-    public int getNextDir(Node to, boolean approach) { return location.getNextDir(to, approach); }
-    public List<Node> getPathTo(Node to) { return getPathTo(to, true); }
-    public List<Node> getPossibleLocations(boolean canReverse) { return super.getPossibleLocations(canReverse); }
-    public Actor getTargetActor(List<? extends Actor> targets, boolean nearest) { return getTargetActor(targets, nearest, true); }
-    public Node getTargetNode(List<Node> targets, boolean nearest) { return getTargetNode(targets, nearest, true); }
-
-    public _Attacker(_Node location, int direction)
-    {
-        super(location, direction);
-    }
-    protected _Attacker clone() { return (_Attacker)super.clone(); }
-}
->>>>>>> 4d5bec6eb2cbe3792fa7dbdb835e789babcdf83f
--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ufl.cs1.controllers;
 
 import game.controllers.DefenderController;
@@ -7,24 +7,22 @@ import game.models.Game;
 
 import java.util.List;
 
-public final class StudentController implements DefenderController
-{
-	public void init(Game game) { }
+public final class StudentController implements DefenderController {
+	public void init(Game game) {
+	}
 
-	public void shutdown(Game game) { }
+	public void shutdown(Game game) {
+	}
 
-	public int[] update(Game game,long timeDue)
-	{
+	public int[] update(Game game, long timeDue) {
 
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
 
 
-
 		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
+		for (int i = 0; i < actions.length; i++) {
 			Defender defender = enemies.get(i);
 			List<Integer> possibleDirs = defender.getPossibleDirs();
 			/*if (possibleDirs.size() != 0 && (i % 2 == 1))
@@ -36,64 +34,22 @@ public final class StudentController implements DefenderController
 			int attackerY = game.getAttacker().getLocation().getY();
 			int defenderX = defender.getLocation().getX();
 			int defenderY = defender.getLocation().getY();
-			if(possibleDirs.size() != 0){
-				if(attackerX >= defenderX)
+			if (possibleDirs.size() != 0) {
+				if (attackerX >= defenderX)
 					actions[i] = 1;
 
-				else if(attackerX <= defenderX)
+				/*else if (attackerX <= defenderX)
 					actions[i] = 3;
-				else if(attackerY >= defenderY)
+					*/
+				else if (attackerY >= defenderY)
 					actions[i] = 0;
-				else if(attackerY <= defenderY)
+				/*else if (attackerY <= defenderY)
 					actions[i] = 2;
+					*/
 			}
+
 
 		}
 		return actions;
 	}
-=======
-package ufl.cs1.controllers;
-
-import game.controllers.DefenderController;
-import game.models.Defender;
-import game.models.Game;
-
-import java.util.List;
-
-public final class StudentController implements DefenderController
-{
-	public void init(Game game) { }
-
-	public void shutdown(Game game) { }
-
-	public int[] update(Game game,long timeDue)
-	{
-
-		int[] actions = new int[Game.NUM_DEFENDER];
-		List<Defender> enemies = game.getDefenders();
-
-
-
-		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
-		//any random number of all of the ghosts
-		for(int i = 0; i < actions.length; i++)
-		{
-			Defender defender = enemies.get(i);
-			List<Integer> possibleDirs = defender.getPossibleDirs();
-			/*if (possibleDirs.size() != 0 && (i % 2 == 1))
-				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
-			else
-				actions[i] = -1;
-				*/
-			if(possibleDirs.size() != 0){
-				actions[0] = 1;
-				actions[1] = 2;
-				actions[2] = 1;
-				actions[3] = 2;
-			}
-
-		}
-		return actions;
-	}
->>>>>>> 4d5bec6eb2cbe3792fa7dbdb835e789babcdf83f
 }
